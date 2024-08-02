d735b66f8b7381a26baee24febf95371a3a5542f
mario <anserteampro@gmail.com>
2024-08-02 10:03:55 +0700

    Keep update

--- keep_update.sh
+++ keep_update.sh
@@ -4,7 +4,7 @@ CUR_DIR=$(pwd)
 
 echo "Run at $CUR_DIR"
 
-date +"%Y-%m-%d %H:%M:%S" > "$CUR_DIR/README.txt"
+date +"%Y-%m-%d %H:%M:%S" > "$CUR_DIR/README.md"
 
 git add .
 git commit -m "Keep update"
